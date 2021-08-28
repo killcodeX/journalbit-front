1cb7dcad1fcb6aa6b2a97e6264f8f23fa462f45b
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-28 21:07:15 +0530

    updated base url

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,8 @@ const openNotificationWithIcon = (type, title, message) => {
   });
 };
 
-export const ApiFunc = axios.create({ baseURL: "http://localhost:5000" });
+
+export const ApiFunc = axios.create({ baseURL: process.env.NODE_ENV ==='development'? "http://localhost:5000": "https://journal-bit-back.vercel.app" });
 
 ApiFunc.interceptors.request.use((req) => {
   if (localStorage.getItem("journaltoken")) {
